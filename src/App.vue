9125b80ee8bc6ba67d9573c156ef825b0ad0f02e
Vladimir <dvvinfo@mail.ru>
2025-01-18 14:07:12 +0300

    initializing the prototype

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class=" container mx-auto">
+  <RouterView />
+</div>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1 class="text-3xl font-bold">Home</h1>
+    <p>Welcome to the Home page!</p>
+  </main>
+</template>
