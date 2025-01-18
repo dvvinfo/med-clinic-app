310e21971c47107714b49713e5960a2de2938a3f
Vladimir <dvvinfo@mail.ru>
2025-01-18 14:33:09 +0300

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import AppHeader from '@/components/AppHeader.vue'
 </script>
 
 <template>
   <div class=" container mx-auto">
+    <AppHeader/>
   <RouterView />
 </div>
 </template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import LogoIcon from '@/components/icons/LogoIcon.vue'
+import AppNavbar from '@/components/AppNavbar.vue'
+</script>
+
+<template>
+  <div class="py-4">
+    <div class="flex items-center space-x-4">
+      <LogoIcon class="h-8" />
+      <AppNavbar />
+    </div>
+  </div>
+</template>
+
+<style></style>
